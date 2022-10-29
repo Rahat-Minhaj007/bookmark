dd833e262be6983c12b58de210f82e639540f1b0
Rahat-Minhaj007 <rahatminhaj555@gmail.com>
2022-10-29 17:57:42 +0600

    brushup

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Bookmarks = () => {
   const [data, setData] = useState([]);
