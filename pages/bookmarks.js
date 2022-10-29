ffafb41138ac9ed62bd77f6378a61d60e84f244d
Rahat-Minhaj007 <rahatminhaj555@gmail.com>
2022-10-29 17:37:26 +0600

    added bookmarks page

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -1,8 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Bookmarks = () => {
   return (
     <div>
+      <div className="bg-primary">
+        <ul className="flex justify-end space-x-8 p-5 text-white font-[500] text-[14px]">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/bookmarks">Bookmarks</Link>
+          </li>
+        </ul>
+      </div>
       <h1>Welcome to Bookmarks</h1>
     </div>
   );
